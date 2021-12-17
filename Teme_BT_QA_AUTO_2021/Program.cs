38d5280cbe780baf34843079ba3c433a7c33eaf0
DraganLoredanaIoana <dragan.loreioana@gmail.com>
2021-12-17 22:11:18 +0200

    Add project files.

---
+++ Teme_BT_QA_AUTO_2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teme_BT_QA_AUTO_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
